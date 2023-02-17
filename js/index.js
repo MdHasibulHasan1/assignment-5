8d39774818a56dc7c42bac4ec57ada9ec46670e6
hasib1511 <hasib7143@gmail.com>
2023-02-18 02:00:38 +0600

    add triangle and ractangle js

---
+++ js/index.js
@@ -0,0 +1,76 @@
+let serial=0;
+// triangle
+document.getElementById('triangle-btn').addEventListener('click', function(){
+    const triangleTitle= getElementFieldInnerText('triangle-title')
+    const triangleBase = getInputFieldValue('triangle-base');
+    const triangleHeight = getInputFieldValue('triangle-height');
+    serial += 1;
+    
+    const areaOfTriangle= 0.5*triangleBase*triangleHeight;
+    displayData(triangleTitle, areaOfTriangle);
+    
+
+})
+// rectangle
+document.getElementById('rectangle-btn').addEventListener('click', function(){
+    const rectangleTitle= getElementFieldInnerText('rectangle-title')
+    const rectangleWidth = getInputFieldValue('rectangle-width');
+    const rectangleLength = getInputFieldValue('rectangle-length');
+    serial += 1;
+    
+    const areaOfRectangle= rectangleWidth*rectangleLength;
+    displayData(rectangleTitle, areaOfRectangle);
+
+})
+function getInputFieldValue(inputId){
+    const valueInString=document.getElementById(inputId).value;
+    if (isNaN(valueInString) || valueInString==='') {
+        return alert('Enter a number');
+
+    }
+    else{
+        document.getElementById(inputId).value="";
+        return parseInt(valueInString);
+    }
+
+}
+function getElementFieldInnerText(elementId){
+    const innerTextInString=document.getElementById(elementId).innerText;
+    return innerTextInString;
+}
+
+
+
+// common function to display data
+function displayData(title, area) {
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${title}</td>
+      <td>${area}cm <sup>2</sup></td>
+      <td>
+      <button class="px-2 py-3 text-white rounded bg-blue-800">Covert to m<sup>2</sup></button>
+      </td> 
+      
+    `;
+    container.appendChild(tr);
+    
+  }
+
+
+
+
+
+
+
+  function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+console.log(generateRandomColor()); 
+document.body.style.backgroundColor= generateRandomColor();
